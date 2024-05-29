231e16da951eb029a2e9f211251b6ce82696e886
NatDavies06 <153016845+NatDavies06@users.noreply.github.com>
2024-05-29 00:40:09 +0000

    added 'not found error' need to update navbar still

---
+++ client/src/componets/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const NotFound = ({ handleModalClose }) => {
+  return (
+    <div className="text-center">
+      <h1>404</h1>
+      <p>Page Not Found</p>
+      <Button variant="secondary" onClick={handleModalClose}>
+        Close
+      </Button>
+    </div>
+  );
+};
+
+export default NotFound;
