7aef5287c2abf01df4bbcba7be2def7b8baa4cfe
Joe <joeyndiebel@gmail.com>
2024-06-03 18:45:03 -0500

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,37 +3,37 @@ function NavBar () {
   <>
     <div id="header">
     <h1 id="logo"><a href="#">MovieHunter</a></h1>
-    <div class="social"> <span>FOLLOW US ON:</span>
+    {/* <div class="social"> <span>FOLLOW US ON:</span>
       <ul>
         <li><a class="twitter" href="#">twitter</a></li>
         <li><a class="facebook" href="#">facebook</a></li>
         <li><a class="vimeo" href="#">vimeo</a></li>
         <li><a class="rss" href="#">rss</a></li>
       </ul>
-    </div>
+    </div> */}
     <div id="navigation">
       <ul>
         <li><a class="active" href="#">HOME</a></li>
-        <li><a href="#">NEWS</a></li>
-        <li><a href="#">IN THEATERS</a></li>
-        <li><a href="#">COMING SOON</a></li>
-        <li><a href="#">CONTACT</a></li>
-        <li><a href="#">ADVERTISE</a></li>
+        <li><a href="#">LOGIN</a></li>
+        <li><a href="#">SIGN UP</a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#">ACCOUNT</a></li>
+        <li><a href="#"></a></li>
       </ul>
     </div>
     <div id="sub-navigation">
-      <ul>
+      {/* <ul>
         <li><a href="#">SHOW ALL</a></li>
         <li><a href="#">LATEST TRAILERS</a></li>
         <li><a href="#">TOP RATED</a></li>
         <li><a href="#">MOST COMMENTED</a></li>
-      </ul>
+      </ul> */}
       <div id="search">
-        <form action="#" method="get" accept-charset="utf-8">
+        {/* <form action="#" method="get" accept-charset="utf-8">
           <label for="search-field">SEARCH</label>
           <input type="text" name="search field" value="Enter search here" id="search-field" class="blink search-field"  />
           <input type="submit" value="GO!" class="search-button" />
-        </form>
+        </form> */}
       </div>
     </div>
   </div>
