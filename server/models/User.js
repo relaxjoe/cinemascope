fac68ded814fd6e9b88fb5281d6f43ca1794a8f0
Owen Johnson <opjoh@gmail.com>
2024-06-03 21:45:00 -0500

    added hash hook

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: { 
@@ -15,6 +16,18 @@ const userSchema = new mongoose.Schema({
         required: true,
      },
 });
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
