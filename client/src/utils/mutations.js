a62af6fd8c3f3e52abbe58195925f4edebae1403
Owen Johnson <opjoh@gmail.com>
2024-06-06 20:26:04 -0500

    deleted extra files

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,7 +26,7 @@ export const CREATE_USER = gql`
 
 export const CREATE_REVIEW = gql`
   mutation createReview($rating: Int!, $comment: String, $title: String!, $director: String, $actors: String) {
-    createReview(input: { rating: $rating, commment: $comment, title: $title, director: $director, actors: $actors }) {
+    createReview(rating: $rating, commment: $comment, title: $title, director: $director, actors: $actors) {
       _id
       rating
       comment
